addc665c0c2a6d736f2661ba51ff8a58173a50b0
billythebean <musiclover7779@gmail.com>
2022-06-29 21:55:28 -0400

    Initial commit

---
+++ cyber_project_python/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for cyber_project_python."""
+
+from .cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ tests/test_base.py
@@ -0,0 +1,5 @@
+from cyber_project_python.base import NAME
+
+
+def test_base():
+    assert NAME == "cyber_project_python"
